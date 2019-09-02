d004f3e8d457a7e5f8ee7794425b6a8a6afba0f7
Leyberson <leyberson1@gmail.com>
2019-09-02 10:03:43 -0300

    Board class working

---
+++ Board.py
@@ -0,0 +1,90 @@
+import numpy as np
+from random import randint
+
+class Board:
+    def __init__(self, dimension):
+        self.dimension = dimension
+        self.board = []
+        aux_board = []
+        for row in range(0, dimension):
+            for column in range(0, dimension):
+                aux_board.append((row*dimension) + column + 1)
+            self.board.append(aux_board)
+            aux_board = []
+        del(aux_board)
+        self.board[dimension - 1][dimension - 1] = 0
+        self.board = np.array(self.board)
+        self.zero_position = [self.dimension - 1, self.dimension - 1]
+    
+    def __str__(self):
+        return str(self.board)
+
+    def __eq__(self, obj):
+        if self.dimension != obj.dimension:
+            return False
+        for row in range(0, self.dimension):
+            for column in range(0, self.dimension):
+                if self.board[row][column] != obj.board[row][column]:
+                    return False
+        return True
+
+    def move_down(self):
+        if self.zero_position[0] > 0:
+            self.board[self.zero_position[0]][self.zero_position[1]] = self.board[self.zero_position[0] - 1][self.zero_position[1]]
+            self.board[self.zero_position[0] - 1][self.zero_position[1]] = 0
+            self.zero_position[0] -= 1
+
+    def move_up(self):
+        if self.zero_position[0] < self.dimension - 1:
+            self.board[self.zero_position[0]][self.zero_position[1]] = self.board[self.zero_position[0] + 1][self.zero_position[1]]
+            self.board[self.zero_position[0] + 1][self.zero_position[1]] = 0
+            self.zero_position[0] += 1
+
+    def move_left(self):
+        if self.zero_position[1] < self.dimension - 1:
+            self.board[self.zero_position[0]][self.zero_position[1]] = self.board[self.zero_position[0]][self.zero_position[1] + 1]
+            self.board[self.zero_position[0]][self.zero_position[1] + 1] = 0
+            self.zero_position[1] += 1
+
+    def move_right(self):
+        if self.zero_position[1] > 0:
+            self.board[self.zero_position[0]][self.zero_position[1]] = self.board[self.zero_position[0]][self.zero_position[1] - 1]
+            self.board[self.zero_position[0]][self.zero_position[1] - 1] = 0
+            self.zero_position[1] -= 1
+
+    def manhatan_distance_target(self):
+        distance = 0
+        for row in range(0, self.dimension):
+            for column in range(0, self.dimension):
+                if self.board[row][column] != 0:
+                    aux = self.board[row][column] - 1
+                    target_position = [aux // self.dimension , aux % self.dimension]
+                    distance += (abs(target_position[0] - row) + abs(target_position[1] - column))
+        return distance
+
+    def shuffle(self, number_of_moviments = randint(1000,2000)):
+        for i in range(0, number_of_moviments):
+            next_moviment = randint(1,4)
+            if next_moviment == 1:
+                self.move_down()
+            elif next_moviment == 2:
+                self.move_up()
+            elif next_moviment == 3:
+                self.move_left()
+            elif next_moviment == 4:
+                self.move_right()
+        
+
+
+
+
+target = Board(5)
+# board = Board(3)
+print(target)
+# print(board)
+# print(target == board)
+print(target.manhatan_distance_target())
+target.shuffle()
+print(target)
+print(target.manhatan_distance_target())
+# print(target == board)
